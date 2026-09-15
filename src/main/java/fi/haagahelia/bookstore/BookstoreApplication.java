d8b96b85d6536a74ff3201670ab9031b4c9fb196
Samir-BK <bhu041@myy.haaga-helia.fi>
2026-09-15 10:45:45 +0300

    Add commnadline runner

--- src/main/java/fi/haagahelia/bookstore/BookstoreApplication.java
+++ src/main/java/fi/haagahelia/bookstore/BookstoreApplication.java
@@ -1,7 +1,12 @@
 package fi.haagahelia.bookstore;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import fi.haagahelia.bookstore.domain.Book;
+import fi.haagahelia.bookstore.domain.BookRepository;
 
 @SpringBootApplication
 public class BookstoreApplication {
@@ -10,4 +15,14 @@ public class BookstoreApplication {
 		SpringApplication.run(BookstoreApplication.class, args);
 	}
 
+	@Bean
+	public CommandLineRunner demo(BookRepository repository) {
+	return (args) -> {
+		repository.save(new Book("Artificial Intelligence in Practice", "Maya Chen", 2026, "9781000000001", 24.99));
+		repository.save(new Book("Cloud Native Architecture", "Liam Johnson", 2026, "9781000000002", 29.99));
+		repository.save(new Book("Cybersecurity for Everyone", "Ava Williams", 2026, "9781000000003", 22.99));
+		repository.save(new Book("The Future of Quantum Computing", "Noah Patel", 2026, "9781000000004", 31.99));
+		repository.save(new Book("Sustainable Software Engineering", "Sofia Garcia", 2026, "9781000000005", 26.99));
+	};
 }
+}	
