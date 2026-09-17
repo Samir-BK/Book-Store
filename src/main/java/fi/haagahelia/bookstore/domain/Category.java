526514323b5fbceb82d07c927ce50627a31c74a6
Samir-BK <bhu041@myy.haaga-helia.fi>
2026-09-17 10:24:53 +0300

    Add category

---
+++ src/main/java/fi/haagahelia/bookstore/domain/Category.java
@@ -0,0 +1,58 @@
+package fi.haagahelia.bookstore.domain;
+
+import java.util.List;
+import java.util.Locale;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+
+@Entity
+public class Category {
+    @Id
+    @GeneratedValue(strategy=GenerationType.AUTO)
+	private Long categoryid;
+	private String name;
+	
+	@OneToMany(cascade = CascadeType.ALL, mappedBy = "department")
+	private List<Book> books;
+	
+	public Category() {}
+	
+	public Category(String name) {
+		super();
+		this.name = name;
+	}
+	
+	public Long getCategoryid() {
+		return categoryid;
+	}	
+	
+	public void setcategoryid(Long departmentid) {
+		this.categoryid = departmentid;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public List<Book> getbooks() {
+		return books;
+	}
+
+	public void setbooks(List<Book> students) {
+		this.books = books;
+	}
+
+	@Override
+	public String toString() {
+		return "Category [category=" + categoryid + ", name=" + name + "]";
+	}
+}
